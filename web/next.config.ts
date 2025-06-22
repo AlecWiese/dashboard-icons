c2d345afe16803258a3af7a608972d6f7d91019e
Alec Wiese <1350676+AlecWiese@users.noreply.github.com>
2025-06-21 23:15:58 -0600

    added docker deploy files
    Signed-off-by: Alec Wiese <1350676+AlecWiese@users.noreply.github.com>

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,10 +1,10 @@
-import type { NextConfig } from "next"
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-	images: {
-		unoptimized: true,
-	},
-	output: "export",
+  output: 'standalone',
+  experimental: {
+    outputFileTracingRoot: undefined,
+  },
 }
 
 export default nextConfig
